baa3b6be0756c4f8c5824425b4612290f3c049f5
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-09-30 21:04:57 +0300

    added all network use cases to di

---
+++ app/src/main/java/ru/sogya/projects/activity_and_charity/di/repository/NetworkModule.kt
@@ -0,0 +1,77 @@
+package ru.sogya.projects.activity_and_charity.di.repository
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import ru.sogya.projects.activityandcharity.domain.repository.NetworkRepository
+import ru.sogya.projects.activityandcharity.domain.usecase.network.achievement.GetAchievementById
+import ru.sogya.projects.activityandcharity.domain.usecase.network.activity.GetAllActivitiesUseCase
+import ru.sogya.projects.activityandcharity.domain.usecase.network.activitystat.GetActivityStatisticByIdUseCase
+import ru.sogya.projects.activityandcharity.domain.usecase.network.auth.CreateUserUseCase
+import ru.sogya.projects.activityandcharity.domain.usecase.network.auth.LoginUserUseCase
+import ru.sogya.projects.activityandcharity.domain.usecase.network.departments.GetAllDepartmentsUseCase
+import ru.sogya.projects.activityandcharity.domain.usecase.network.departments.GetDepartmentByIdUseCase
+import ru.sogya.projects.activityandcharity.domain.usecase.network.fund.GetAllFundUseCase
+import ru.sogya.projects.activityandcharity.domain.usecase.network.fund.GetFundByIdUseCase
+import ru.sogya.projects.activityandcharity.domain.usecase.network.rating.GetUserRatingUseCase
+import ru.sogya.projects.activityandcharity.domain.usecase.network.user.GetAllUsersUseCase
+import ru.sogya.projects.activityandcharity.domain.usecase.network.user.GetUserByIdUseCase
+import ru.sogya.projects.activityandcharity.domain.usecase.network.userstat.GetUserStatisticByIdUseCase
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class NetworkModule {
+
+    @Provides
+    fun provideCreateUserUseCase(networkRepository: NetworkRepository) =
+        CreateUserUseCase(networkRepository)
+
+    @Provides
+    fun provideGetAchievementByIdUseCase(networkRepository: NetworkRepository) =
+        GetAchievementById(networkRepository)
+
+    @Provides
+    fun provideGetAllActivitiesUseCase(networkRepository: NetworkRepository) =
+        GetAllActivitiesUseCase(networkRepository)
+
+    @Provides
+    fun provideGetActivityStatisticByIdUseCase(networkRepository: NetworkRepository) =
+        GetActivityStatisticByIdUseCase(networkRepository)
+
+    @Provides
+    fun provideLoginUserUseCase(networkRepository: NetworkRepository) =
+        LoginUserUseCase(networkRepository)
+
+    @Provides
+    fun provideGetAllDepartmentsUseCase(networkRepository: NetworkRepository) =
+        GetAllDepartmentsUseCase(networkRepository)
+
+    @Provides
+    fun provideGetDepartmentByIdUseCase(networkRepository: NetworkRepository) =
+        GetDepartmentByIdUseCase(networkRepository)
+
+    @Provides
+    fun provideGetAllFundUseCase(networkRepository: NetworkRepository) =
+        GetAllFundUseCase(networkRepository)
+
+    @Provides
+    fun provideGetFundByIdUseCase(networkRepository: NetworkRepository) =
+        GetFundByIdUseCase(networkRepository)
+
+    @Provides
+    fun provideGetUserRatingUseCase(networkRepository: NetworkRepository) =
+        GetUserRatingUseCase(networkRepository)
+
+    @Provides
+    fun provideGetAllUsersUseCase(networkRepository: NetworkRepository) =
+        GetAllUsersUseCase(networkRepository)
+
+    @Provides
+    fun provideGetUserByIdUseCase(networkRepository: NetworkRepository) =
+        GetUserByIdUseCase(networkRepository)
+
+    @Provides
+    fun provideGetUserStatisticByIdUseCase(networkRepository: NetworkRepository) =
+        GetUserStatisticByIdUseCase(networkRepository)
+}
