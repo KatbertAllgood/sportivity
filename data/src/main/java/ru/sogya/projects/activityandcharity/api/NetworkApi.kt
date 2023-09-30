081d82f2ea6c9c4f001ca85b10f0d80a1d3f7fdb
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-09-30 13:14:30 +0300

    added test get fun

--- data/src/main/java/ru/sogya/projects/activityandcharity/api/NetworkApi.kt
+++ data/src/main/java/ru/sogya/projects/activityandcharity/api/NetworkApi.kt
@@ -1,5 +1,11 @@
 package ru.sogya.projects.activityandcharity.api
 
+import kotlinx.coroutines.flow.Flow
+import retrofit2.http.GET
+import ru.sogya.projects.activityandcharity.model.ActivityData
+
 interface NetworkApi {
 
+    @GET()
+    fun getAllActivities(): Flow<List<ActivityData>>
 }
