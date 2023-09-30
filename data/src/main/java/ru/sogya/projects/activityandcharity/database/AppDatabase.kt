db000ed703034014b1b78ddb97d0a3ae23320b6a
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-09-30 13:14:16 +0300

    added daos to database control class

--- data/src/main/java/ru/sogya/projects/activityandcharity/database/AppDatabase.kt
+++ data/src/main/java/ru/sogya/projects/activityandcharity/database/AppDatabase.kt
@@ -2,13 +2,18 @@ package ru.sogya.projects.activityandcharity.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import ru.sogya.projects.activityandcharity.model.TestModel
+import ru.sogya.projects.activityandcharity.database.dao.ActivityDao
+import ru.sogya.projects.activityandcharity.database.dao.ActivityStatisticDao
+import ru.sogya.projects.activityandcharity.database.dao.UserDao
+import ru.sogya.projects.activityandcharity.database.dao.UserStatisticDao
+import ru.sogya.projects.activityandcharity.model.UserData
 
 @Database(
-    entities = [
-        TestModel::class
-               ],
-    version = 1
+    entities = [UserData::class], version = 1
 )
 abstract class LocalDataBase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+    abstract fun activityDao(): ActivityDao
+    abstract fun userStatisticDao(): UserStatisticDao
+    abstract fun activityStatisticDao(): ActivityStatisticDao
 }
