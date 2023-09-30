66654b7a6d820717bbad6e2b6294839b6fb69473
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-09-30 22:25:10 +0300

    fixeb bug

--- domain/src/main/java/ru/sogya/projects/activityandcharity/domain/usecase/network/user/GetUserByIdUseCase.kt
+++ domain/src/main/java/ru/sogya/projects/activityandcharity/domain/usecase/network/user/GetUserByIdUseCase.kt
@@ -3,5 +3,5 @@ package ru.sogya.projects.activityandcharity.domain.usecase.network.user
 import ru.sogya.projects.activityandcharity.domain.repository.NetworkRepository
 
 class GetUserByIdUseCase(private val networkRepository: NetworkRepository) {
-    operator fun invoke() = networkRepository.getUserById()
+    operator fun invoke(userid:Int) = networkRepository.getUserById(userid)
 }
