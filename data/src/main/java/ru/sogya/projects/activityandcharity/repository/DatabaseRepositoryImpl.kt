490cd3a6fdd5fd2754c359f72f5282ee22f6e92b
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-09-30 13:39:22 +0300

    added all database fun's

--- data/src/main/java/ru/sogya/projects/activityandcharity/repository/DatabaseRepositoryImpl.kt
+++ data/src/main/java/ru/sogya/projects/activityandcharity/repository/DatabaseRepositoryImpl.kt
@@ -1,7 +1,71 @@
 package ru.sogya.projects.activityandcharity.repository
 
 import android.content.Context
+import kotlinx.coroutines.flow.Flow
+import ru.sogya.projects.activityandcharity.domain.model.ActivityDomain
+import ru.sogya.projects.activityandcharity.domain.model.ActivityStatisticDomain
+import ru.sogya.projects.activityandcharity.domain.model.UserDomain
+import ru.sogya.projects.activityandcharity.domain.model.UserStatisticDomain
 import ru.sogya.projects.activityandcharity.domain.repository.DatabaseRepository
 
 class DatabaseRepositoryImpl(context: Context) : DatabaseRepository {
+    override fun getAllActivities(): Flow<List<ActivityDomain>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun insertActivities(activities: List<ActivityDomain>): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateActivities(activities: List<ActivityDomain>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getActivityStatisticDomain(): Flow<ActivityStatisticDomain> {
+        TODO("Not yet implemented")
+    }
+
+    override fun insertActivityStatistic(activityStatisticData: ActivityStatisticDomain): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateActivityStatistic(activityStatisticData: ActivityStatisticDomain): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteActivityStatistic(activityStatisticData: ActivityStatisticDomain): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUser(): Flow<UserDomain> {
+        TODO("Not yet implemented")
+    }
+
+    override fun insertUser(userData: UserDomain): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateUser(userData: UserDomain): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteUser(userData: UserDomain): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserStatistic(): Flow<UserStatisticDomain> {
+        TODO("Not yet implemented")
+    }
+
+    override fun insertUserStatistic(userStatisticData: UserStatisticDomain): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateUserStatistic(userStatisticData: UserStatisticDomain): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteUserStatistic(userStatisticData: UserStatisticDomain): Int {
+        TODO("Not yet implemented")
+    }
 }
--- domain/src/main/java/ru/sogya/projects/activityandcharity/domain/repository/DatabaseRepository.kt
+++ domain/src/main/java/ru/sogya/projects/activityandcharity/domain/repository/DatabaseRepository.kt
@@ -1,4 +1,40 @@
 package ru.sogya.projects.activityandcharity.domain.repository
 
+import kotlinx.coroutines.flow.Flow
+import ru.sogya.projects.activityandcharity.domain.model.ActivityDomain
+import ru.sogya.projects.activityandcharity.domain.model.ActivityStatisticDomain
+import ru.sogya.projects.activityandcharity.domain.model.UserDomain
+import ru.sogya.projects.activityandcharity.domain.model.UserStatisticDomain
+
 interface DatabaseRepository {
+
+    fun getAllActivities(): Flow<List<ActivityDomain>>
+
+    fun insertActivities(activities: List<ActivityDomain>): Long
+
+    fun updateActivities(activities: List<ActivityDomain>): Int
+
+    fun getActivityStatisticDomain(): Flow<ActivityStatisticDomain>
+
+    fun insertActivityStatistic(activityStatisticData: ActivityStatisticDomain): Long
+
+    fun updateActivityStatistic(activityStatisticData: ActivityStatisticDomain): Int
+
+    fun deleteActivityStatistic(activityStatisticData: ActivityStatisticDomain): Int
+
+    fun getUser(): Flow<UserDomain>
+
+    fun insertUser(userData: UserDomain): Long
+
+    fun updateUser(userData: UserDomain): Int
+
+    fun deleteUser(userData: UserDomain): Int
+
+    fun getUserStatistic(): Flow<UserStatisticDomain>
+
+    fun insertUserStatistic(userStatisticData: UserStatisticDomain): Long
+
+    fun updateUserStatistic(userStatisticData: UserStatisticDomain): Int
+
+    fun deleteUserStatistic(userStatisticData: UserStatisticDomain): Int
 }
