123a2cd2ec7ecdc67b928f95e04ab7c58108deee
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-09-30 17:18:58 +0300

    renamed

--- domain/src/main/java/ru/sogya/projects/activityandcharity/domain/usecase/database/activity/GetAllActivitiesUseCase.kt
+++ domain/src/main/java/ru/sogya/projects/activityandcharity/domain/usecase/database/activity/GetAllActivitiesUseCase.kt
@@ -2,6 +2,6 @@ package ru.sogya.projects.activityandcharity.domain.usecase.database.activity
 
 import ru.sogya.projects.activityandcharity.domain.repository.DatabaseRepository
 
-class GetAllActivitieUseCase(private val databaseRepository: DatabaseRepository) {
+class GetAllActivitiesUseCase(private val databaseRepository: DatabaseRepository) {
     operator fun invoke() = databaseRepository.getAllActivities()
 }
