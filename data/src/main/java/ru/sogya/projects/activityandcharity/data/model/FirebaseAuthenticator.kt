9f0d55fadba0e6c2bd25551aa866d12d6e506259
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-12-06 16:00:13 +0300

    added authenticator support class

---
+++ data/src/main/java/ru/sogya/projects/activityandcharity/data/model/FirebaseAuthenticator.kt
@@ -0,0 +1,16 @@
+package ru.sogya.projects.activityandcharity.data.model
+
+import com.google.firebase.auth.FirebaseAuth
+import javax.inject.Inject
+
+class FirebaseAuthenticator @Inject constructor(private val firebaseAuth: FirebaseAuth) {
+    fun signUpWithEmailPassword(email: String, password: String) =
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+
+    fun signInWithEmailPassword(email: String, password: String) =
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+
+    fun signOut() = firebaseAuth.signOut()
+
+    fun sendPasswordReset(email: String) = firebaseAuth.sendPasswordResetEmail(email)
+}
