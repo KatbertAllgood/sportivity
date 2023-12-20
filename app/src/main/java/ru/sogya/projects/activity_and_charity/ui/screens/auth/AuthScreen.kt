1197eded05d885a3608a919a81e48d5fffb6e175
KatbertAllgood <srazmi@mail.ru>
2023-12-20 20:20:05 +0300

    add registration and auth to unit file

--- app/src/main/java/ru/sogya/projects/activity_and_charity/ui/screens/auth/AuthScreen.kt
+++ app/src/main/java/ru/sogya/projects/activity_and_charity/ui/screens/auth/AuthScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -20,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,13 +29,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.compose.rememberNavController
 import ru.sogya.projects.activity_and_charity.R
-import ru.sogya.projects.activity_and_charity.ui.navigation.BottomBarScreen
 import ru.sogya.projects.activity_and_charity.ui.theme.ActivityAndCharityTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -42,7 +42,52 @@ fun AuthScreenComposable(
     onClick: () -> Unit
 ) {
 
+    // begin / auth / reg
+
+    var authMode by rememberSaveable {
+        mutableStateOf("begin")
+    }
+
+    var isButtonEnabled by rememberSaveable{
+        mutableStateOf(false)
+    }
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    var fio by remember {
+        mutableStateOf("")
+    }
+
+    var nickname by remember {
+        mutableStateOf("")
+    }
+
     ActivityAndCharityTheme {
+
+
+        val textFieldColors = TextFieldDefaults.colors(
+            focusedTextColor = ActivityAndCharityTheme.colors.white,
+            unfocusedTextColor = ActivityAndCharityTheme.colors.white,
+            focusedContainerColor = ActivityAndCharityTheme.colors.secondary,
+            unfocusedContainerColor = ActivityAndCharityTheme.colors.secondary,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedLabelColor = ActivityAndCharityTheme.colors.accent,
+            unfocusedLabelColor = ActivityAndCharityTheme.colors.accent,
+            cursorColor = ActivityAndCharityTheme.colors.accent
+        )
+
+        val textFieldTextStyle = TextStyle(
+            fontFamily = ActivityAndCharityTheme.typography.regular.fontFamily,
+            fontSize = ActivityAndCharityTheme.typography.regular.fontSize
+        )
+
         Column(
             modifier = Modifier
                 .background(
@@ -60,8 +105,16 @@ fun AuthScreenComposable(
                     )
                     .size(60.dp)
             )
+
             Text(
-                text = stringResource(id = R.string.sing_up_or_in),
+                text = stringResource(
+                    when(authMode) {
+                        "begin" -> R.string.sing_up_or_in
+                        "auth" -> R.string.entrance
+                        "reg" -> R.string.registration
+                        else -> 0
+                    }
+                ),
                 fontFamily = ActivityAndCharityTheme.typography.regularMedium.fontFamily,
                 fontSize = 20.sp,
                 color = ActivityAndCharityTheme.colors.white,
@@ -71,143 +124,155 @@ fun AuthScreenComposable(
                     )
             )
 
-            var email by remember {
-                mutableStateOf(
-                    TextFieldValue("")
-                )
-            }
-
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        top = 24.dp,
-                        start = 16.dp,
-                        end = 16.dp
-                    ),
-                colors = CardDefaults.cardColors(
-                    containerColor = ActivityAndCharityTheme.colors.secondary
-                ),
-                shape = ActivityAndCharityTheme.shape.cornersStyle,
-                elevation = CardDefaults.cardElevation(
-                    defaultElevation = 0.dp
-                )
-            ) {
+            CardForTf {
                 TextField(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(60.dp),
+                        .height(52.dp),
                     value = email,
                     onValueChange = { newText ->
                         email = newText
+
+                        when (authMode) {
+                            "begin" -> isButtonEnabled = (newText != "")
+                            "auth" -> isButtonEnabled = (newText != "" && password != "")
+                            "reg" -> isButtonEnabled = (newText != "" && password != ""
+                                    && fio != "" && nickname != "")
+                        }
                     },
                     singleLine = true,
-                    colors = TextFieldDefaults.colors(
-                        focusedTextColor = ActivityAndCharityTheme.colors.white,
-                        unfocusedTextColor = ActivityAndCharityTheme.colors.white,
-                        focusedContainerColor = ActivityAndCharityTheme.colors.secondary,
-                        unfocusedContainerColor = ActivityAndCharityTheme.colors.secondary,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                        focusedLabelColor = ActivityAndCharityTheme.colors.accent,
-                        unfocusedLabelColor = ActivityAndCharityTheme.colors.accent,
-                        cursorColor = ActivityAndCharityTheme.colors.accent
-                    ),
-                    label = {
-                        Text(
-                            text = stringResource(id = R.string.type_email),
-                            fontFamily = ActivityAndCharityTheme.typography.regular.fontFamily,
-                            fontSize = 17.sp
-                        )
-                    },
+                    colors = textFieldColors,
+                    label = { TfLabel(textId = R.string.type_email) },
                     shape = ActivityAndCharityTheme.shape.cornersStyle,
-                    textStyle = TextStyle(
-                        fontFamily = ActivityAndCharityTheme.typography.regular.fontFamily
-                    )
+                    textStyle = textFieldTextStyle,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
                 )
             }
 
-            var password by remember {
-                mutableStateOf(
-                    TextFieldValue("")
-                )
-            }
-
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        top = 24.dp,
-                        start = 16.dp,
-                        end = 16.dp
-                    ),
-                colors = CardDefaults.cardColors(
-                    containerColor = ActivityAndCharityTheme.colors.secondary
-                ),
-                shape = ActivityAndCharityTheme.shape.cornersStyle,
-                elevation = CardDefaults.cardElevation(
-                    defaultElevation = 0.dp
-                )
-            ) {
-                TextField(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(60.dp),
-                    value = email,
-                    onValueChange = { newText ->
-                        email = newText
-                    },
-                    singleLine = true,
-                    colors = TextFieldDefaults.colors(
-                        focusedTextColor = ActivityAndCharityTheme.colors.white,
-                        unfocusedTextColor = ActivityAndCharityTheme.colors.white,
-                        focusedContainerColor = ActivityAndCharityTheme.colors.secondary,
-                        unfocusedContainerColor = ActivityAndCharityTheme.colors.secondary,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                        focusedLabelColor = ActivityAndCharityTheme.colors.accent,
-                        unfocusedLabelColor = ActivityAndCharityTheme.colors.accent,
-                        cursorColor = ActivityAndCharityTheme.colors.accent
-                    ),
-                    label = {
-                        Text(
-                            text = stringResource(id = R.string.type_pass),
-                            fontFamily = ActivityAndCharityTheme.typography.regular.fontFamily,
-                            fontSize = 17.sp
+            when (authMode) {
+                "auth" -> {
+                    CardForTf {
+                        TextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(52.dp),
+                            value = password,
+                            onValueChange = { newText ->
+                                password = newText
+                                isButtonEnabled = (newText != "" && password != "")
+                            },
+                            singleLine = true,
+                            colors = textFieldColors,
+                            label = { TfLabel(textId = R.string.type_pass) },
+                            shape = ActivityAndCharityTheme.shape.cornersStyle,
+                            textStyle = textFieldTextStyle,
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
                         )
-                    },
-                    shape = ActivityAndCharityTheme.shape.cornersStyle,
-                    textStyle = TextStyle(
-                        fontFamily = ActivityAndCharityTheme.typography.regular.fontFamily
-                    )
-                )
+                    }
+                }
+                "reg" -> {
+
+                    CardForTf {
+                        TextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(52.dp),
+                            value = fio,
+                            onValueChange = { newText ->
+                                fio = newText
+                                isButtonEnabled = (newText != "" && password != ""
+                                        && fio != "" && nickname != "")
+                            },
+                            singleLine = true,
+                            colors = textFieldColors,
+                            label = { TfLabel(textId = R.string.fio) },
+                            shape = ActivityAndCharityTheme.shape.cornersStyle,
+                            textStyle = textFieldTextStyle,
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+                        )
+                    }
+
+                    CardForTf {
+                        TextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(52.dp),
+                            value = nickname,
+                            onValueChange = { newText ->
+                                nickname = newText
+                                isButtonEnabled = (newText != "" && password != ""
+                                        && fio != "" && nickname != "")
+                            },
+                            singleLine = true,
+                            colors = textFieldColors,
+                            label = { TfLabel(textId = R.string.nickname) },
+                            shape = ActivityAndCharityTheme.shape.cornersStyle,
+                            textStyle = textFieldTextStyle,
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+                        )
+                    }
+
+                    CardForTf {
+                        TextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(52.dp),
+                            value = password,
+                            onValueChange = { newText ->
+                                password = newText
+                                isButtonEnabled = (newText != "" && password != ""
+                                        && fio != "" && nickname != "")
+                            },
+                            singleLine = true,
+                            colors = textFieldColors,
+                            label = { TfLabel(textId = R.string.password) },
+                            shape = ActivityAndCharityTheme.shape.cornersStyle,
+                            textStyle = textFieldTextStyle,
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+                        )
+                    }
+                }
             }
 
             Button(
                 onClick = {
-                          onClick()
+                    onClick()
+                    when(authMode) {
+                        "begin" -> authMode = "auth"
+                        "auth" -> authMode = "reg"
+                        "reg" -> authMode = "begin"
+                    }
+                    isButtonEnabled = false
                 },
                 shape = ActivityAndCharityTheme.shape.cornersStyle,
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = ActivityAndCharityTheme.colors.accent
+                    containerColor = ActivityAndCharityTheme.colors.accent,
+                    disabledContentColor = ActivityAndCharityTheme.colors.primary,
+                    disabledContainerColor = ActivityAndCharityTheme.colors.accentDisabled
                 ),
+                enabled = isButtonEnabled,
                 modifier = Modifier
                     .fillMaxWidth()
-//                    .height(40.dp)
                     .padding(
                         top = 24.dp,
                         start = 16.dp,
                         end = 16.dp
                     )
+                    .height(44.dp)
             ) {
-
                 Text(
-                    text = stringResource(id = R.string.sing_in),
+                    text = stringResource(
+                        when(authMode) {
+                            "begin" -> R.string.continue_
+                            "auth" -> R.string.sing_in
+                            "reg" -> R.string.sing_up
+                            else -> 0
+                        }
+                    ),
                     color = ActivityAndCharityTheme.colors.primary,
                     fontFamily = ActivityAndCharityTheme.typography.regular.fontFamily,
                     fontSize = 17.sp
                 )
-
             }
         }
     }
@@ -219,4 +284,42 @@ private fun AuthScreenPreview() {
     AuthScreenComposable(
         onClick = { }
     )
+}
+
+@Composable
+private fun TfLabel(
+    textId: Int
+) {
+    ActivityAndCharityTheme {
+        Text(
+            text = stringResource(id = textId),
+            fontFamily = ActivityAndCharityTheme.typography.regular.fontFamily,
+            fontSize = 17.sp,
+            color = ActivityAndCharityTheme.colors.secondaryText
+        )
+    }
+}
+
+@Composable
+private fun CardForTf(
+    content: @Composable () -> Unit
+) {
+    return ActivityAndCharityTheme {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = 24.dp,
+                    start = 16.dp,
+                    end = 16.dp
+                ),
+            colors = CardDefaults.cardColors(
+                containerColor = ActivityAndCharityTheme.colors.secondary
+            ),
+            shape = ActivityAndCharityTheme.shape.cornersStyle,
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 0.dp
+            )
+        ) { content() }
+    }
 }
